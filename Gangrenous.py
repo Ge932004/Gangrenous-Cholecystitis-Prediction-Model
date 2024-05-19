5baff2bbdfa053caa19021412c48b53791ee3e4f
Ge932004 <157493578+Ge932004@users.noreply.github.com>
2024-05-19 22:01:12 +0800

    Update Gangrenous.py

--- Gangrenous.py
+++ Gangrenous.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-!pip install shap
+import subprocess
+subprocess.check_call(["pip", "install", "shap"]
 import shap
 import matplotlib.pyplot as plt
 import joblib
